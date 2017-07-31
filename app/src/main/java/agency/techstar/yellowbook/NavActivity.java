d74dc50efdd024bee5a8aed38ec4b348dedbd1ad
Doljko Dolly <doljko927@gmail.com>
2017-08-01 00:20:42 +0800

    like,share нэмсэн.

--- app/src/main/java/agency/techstar/yellowbook/NavActivity.java
+++ app/src/main/java/agency/techstar/yellowbook/NavActivity.java
@@ -98,17 +98,13 @@ public class NavActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.shake) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.about) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.like) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.share) {
             startActivity(new Intent(NavActivity.this, AboutUsActivity.class));
 
         }
